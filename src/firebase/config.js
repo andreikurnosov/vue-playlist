37395fd80e7905aac088ef193176fdc722d934a9
andreikurnosov <nixdie@yandex.ru>
2021-05-24 12:31:06 +0700

    connecting to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firbase.firestore()
+const projectAuth = firebase.auth()
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
