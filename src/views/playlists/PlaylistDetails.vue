db2ba6f407431ceeae2fc74195ea8b78ad9eb712
andreikurnosov <nixdie@yandex.ru>
2021-05-28 11:54:01 +0700

    playlist details route

---
+++ src/views/playlists/PlaylistDetails.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Playlist Details. ID - {{ id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id']
+}
+</script>
+
+<style lang="scss" scoped></style>
