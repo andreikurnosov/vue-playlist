f58d75bb2d6bf46f49d66662b77e5cb6d3eec431
andreikurnosov <nixdie@yandex.ru>
2021-06-01 00:14:13 +0700

    adding songs to playlists

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,71 +1,74 @@
 <template>
-  <div class="error" v-if="error">
-    {{ error }}
-  </div>
-  <div class="playlist-details" v-if="playlist">
-    <div class="playlist-info">
-      <div class="cover">
-        <img :src="playlist.coverUrl">
-      </div>
-      <h2>{{ playlist.title }} </h2>
-      <p class="username">Created by {{ playlist.userName }}</p>
-      <p class="description">{{ playlist.description }}</p>
-      <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
-    </div>    
-
-    <div class="song-list">
-      song list here
+    <div class="error" v-if="error">
+        {{ error }}
     </div>
-  </div>
+    <div class="playlist-details" v-if="playlist">
+        <div class="playlist-info">
+            <div class="cover">
+                <img :src="playlist.coverUrl">
+            </div>
+            <h2>{{ playlist.title }} </h2>
+            <p class="username">Created by {{ playlist.userName }}</p>
+            <p class="description">{{ playlist.description }}</p>
+            <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
+        </div>
+        <div class="song-list">
+            <add-song v-if="ownership" :playlist="playlist" />
+        </div>
+    </div>
 </template>
-
 <script>
 import useStorage from '@/composables/useStorage'
 import useDocument from '@/composables/useDocument'
 import getDocument from '@/composables/getDocument'
 import getUser from '@/composables/getUser'
-import {computed} from 'vue'
-import {useRouter} from 'vue-router'
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import AddSong from '@/components/AddSong.vue'
 
 export default {
-  props: ['id'],
-  setup(props) {
-    const {error, document: playlist} = getDocument('playlists', props.id)
-    const {user} = getUser()
-    const { deleteDoc } = useDocument('playlists', props.id)
-    const { deleteImage } = useStorage()
-    const router = useRouter()
+    props: ['id'],
+    components: {
+        AddSong
+    },
+    setup(props) {
+        const { error, document: playlist } = getDocument('playlists', props.id)
+        const { user } = getUser()
+        const { deleteDoc } = useDocument('playlists', props.id)
+        const { deleteImage } = useStorage()
+        const router = useRouter()
 
-    const ownership = computed(() => {
-      return playlist.value && user.value && user.value.uid == playlist.value.userId
-    })
+        const ownership = computed(() => {
+            return playlist.value && user.value && user.value.uid == playlist.value.userId
+        })
 
-    const handleDelete = async () => {
-      await deleteImage(playlist.value.filePath)
-      await deleteDoc()
-      
-      router.push({name: 'Home'})
+        const handleDelete = async () => {
+            await deleteImage(playlist.value.filePath)
+            await deleteDoc()
 
+            router.push({ name: 'Home' })
+
+        }
+
+        return { error, playlist, ownership, handleDelete }
     }
-
-    return { error, playlist, ownership, handleDelete }
-  }
 }
 </script>
-
 <style>
-  .playlist-details {
+.playlist-details {
     display: grid;
     grid-template-columns: 1fr 2fr;
     gap: 80px;
-  }
-  .cover {
+}
+
+.cover {
     overflow: hidden;
     border-radius: 20px;
     position: relative;
     padding: 160px;
-  }
-  .cover img {
+}
+
+.cover img {
     display: block;
     position: absolute;
     top: 0;
@@ -74,22 +77,27 @@ export default {
     min-height: 100%;
     max-width: 200%;
     max-height: 200%;
-  }
-  .playlist-info {
+}
+
+.playlist-info {
     text-align: center;
-  }
-  .playlist-info h2 {
+}
+
+.playlist-info h2 {
     text-transform: capitalize;
     font-size: 28px;
     margin-top: 20px;
-  }
-  .playlist-info p {
+}
+
+.playlist-info p {
     margin-bottom: 20px;
-  }
-  .username {
+}
+
+.username {
     color: #999;
-  }
-  .description {
+}
+
+.description {
     text-align: left;
-  }
+}
 </style>
